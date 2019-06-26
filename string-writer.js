96300e9c27c68ffc64dd3d964fb7654a0d17bcb7
Richard Remer <rich.remer@gmail.com>
2019-06-25 20:37:21 -0700

    buffer/decoder support

--- string-writer.js
+++ string-writer.js
@@ -1,28 +1,30 @@
-const {assign} = Object;
 const {Writable} = require("stream");
+const {StringDecoder} = require("string_decoder");
+const {assign} = Object;
 
 /**
- * Create writable stream which evaluates to its concatenated string input.
+ * Create writable stream which evaluates to its concatenated string input.  If
+ * encoding is provided, decode buffer input.
+ * @param {string} [encoding]
  * @returns {Writable}
  */
-function stringWriter() {
-    const chunks = [""];
+function stringWriter(encoding) {
+    const chunks = [];
     const writer = new Writable({write});
+    const decoder = encoding ? new StringDecoder(encoding) : undefined;
 
     return assign(writer, {toString});
 
     function write(chunk, enc, done) {
-        chunks.push(chunk);
+        chunks.push(decoder ? decoder.write(chunk) : chunk);
         done();
     }
 
     function toString() {
-        const len = chunks.length;
+        // concatenate chunks and save concatenated value as single chunk
+        chunks.splice(0, chunks.length, chunks.join(""));
 
-        if (chunks.length > 1) {
-            chunks.splice(0, len, chunks.join(""));
-        }
-
+        // return the concatenated chunk
         return chunks[0];
     }
 }
--- test/test-string-writer.js
+++ test/test-string-writer.js
@@ -20,3 +20,19 @@ describe("stringWriter()", () => {
         expect(String(writer)).to.be("foobar");
     });
 });
+
+describe("stringWriter(string)", () => {
+    it("should decode buffer input", () => {
+        const writer8 = stringWriter("utf8");
+        const euro8 = new Buffer("€", "utf8");
+
+        writer8.write(euro8);
+        expect(String(writer8)).to.be("€");
+
+        const writer16 = stringWriter("utf16le");
+        const euro16 = new Buffer("€", "utf16le");
+
+        writer16.write(euro16);
+        expect(String(writer16)).to.be("€");
+    });
+});
